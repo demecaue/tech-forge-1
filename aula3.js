ae3fa1140e0d84197ed02bf24c56cf1166eb1495
demecaue <151752643+demecaue@users.noreply.github.com>
2026-03-16 22:42:37 -0300

    Add files via upload

---
+++ aula3.js
@@ -0,0 +1,9 @@
+const prompt = require ('prompt-sync')();
+
+let nome = prompt ('Digite seu nome');
+console.log("O nome digitado  foi:" +nome);
+
+
+let cor = prompt ('Digite sua cor favorita');
+console.log(" A cor digitada foi:" +cor);
+
